98244fe1c2048b5033636b6254b22ac105e2141b
Alejandro550r <alejandro550r@gmail.com>
2022-05-14 17:06:39 -0700

    modulo de login adaptado

--- resources/views/auth/contenido.blade.php
+++ resources/views/auth/contenido.blade.php
@@ -1,75 +1,30 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="es">
 <head>
-    <title>Login</title>
-    <link href="application.min.css" rel="stylesheet">
-    <!-- Icons -->
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="all.min.css">
-    <link rel="stylesheet" href="font-awesome.min.css"> -->
-    <!-- Icons -->
-    <!-- Icons -->
-    
-    <link href="css/plantilla.css" rel="stylesheet">
-    <link href="css/application.min.css" rel="stylesheet">
 
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Sistema Ventas Laravel Vue Js">
+  <link rel="shortcut icon" href="vendors/img/favicon.png">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+  <title>Sistema de Gestion de Inventario </title>
 
-    <link rel="shortcut icon" href="signin.svg">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <script async="" src="https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js"></script>
-    <script>
-        /* yeah we need this empty stylesheet here. It's cool chrome & chromium fix
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        chrome fix https://code.google.com/p/chromium/issues/detail?id=167083
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        https://code.google.com/p/chromium/issues/detail?id=332189
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        */
-    </script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-36759672-9"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
+  <link href="css/plantilla.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  
 
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'UA-36759672-9');
-    </script>
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript">
-        (function(m, e, t, r, i, k, a) {
-            m[i] = m[i] || function() {
-                (m[i].a = m[i].a || []).push(arguments)
-            };
-            m[i].l = 1 * new Date();
-            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-        })(window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
-        ym(48020168, "init", {
-            id: 48020168,
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
-            webvisor: true,
-            trackHash: true,
-            ecommerce: "dataLayer"
-        });
-    </script>
-    <!-- /Yandex.Metrika counter -->
 </head>
 
-<body class="login-page">
-    <div class="container-fluid">
-        <!-- main page content. the place to put widgets in. usually consists of .row > .col-lg-* > .widget.  -->
-        @yield('login')
-    </div>
-    <!-- The Loader. Is shown when pjax happens -->
-    <div class="loader-wrap hiding hide ">
-        <i class="fa fa-circle-o-notch fa-spin-fast "></i>
-    </div>
-
-    <!-- Bootstrap and necessary plugins -->
-    <script src="js/plantilla.js"></script>
+<body style="background-color: white;">
+<div id="app">
+  @yield('login')
+</div>
+  <script src="js/app.js"></script>
+  <script src="js/plantilla.js"></script>
+  <script src="js/main.js"></script>
 
 </body>
-
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,79 +11,123 @@
 |
 */
 
-Route::get('/', function () {
-    return view('contenido/contenido');
+Route::group(['middleware' => ['guest']], function(){
+
+    //++++ rutas para el login ++++//
+    Route::get('/', 'Auth\LoginController@showLoginForm');
+    Route::post('/login', 'Auth\LoginController@login')->name('login');
+
 });
 
-//listar todos los datos de la tabla
-Route::get('/categoria', 'CategoriaController@index');
+Route::group(['middleware' => ['auth']], function(){
 
-//insertar registros en la tabla
-Route::post('/categoria/registrar', 'CategoriaController@store');
+        Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
-//insertar registros en la tabla
-Route::put('/categoria/actualizar', 'CategoriaController@update');
-
-//desactivar registros en la tabla
-Route::put('/categoria/desactivar', 'CategoriaController@desactivar');
-
-//activar registros en la tabla
-Route::put('/categoria/activar', 'CategoriaController@activar');
-
-//para seleccionar categoria
-Route::get('/categoria/selectCategoria', 'CategoriaController@selectCategoria');
+        Route::get('/main', function () {
+            return view('contenido/contenido');
+        })->name('main');
 
 
-///   RUTAS DE PRODUCTO ////
-//listar todos los datos de la tabla
-Route::get('/producto', 'ProductoController@index');
+        Route::group(['middleware' => ['Administrador']], function(){
+             
+            //RUTAS DE LAS CATEGORIAS
+            Route::get('/categoria', 'CategoriaController@index');
+            Route::post('/categoria/registrar', 'CategoriaController@store');
+            Route::put('/categoria/actualizar', 'CategoriaController@update');
+            Route::put('/categoria/desactivar', 'CategoriaController@desactivar');
+            Route::put('/categoria/activar', 'CategoriaController@activar');
+            Route::get('/categoria/selectCategoria', 'CategoriaController@selectCategoria');
+            
+            
+            ///   RUTAS DE PRODUCTO ////
+            //listar todos los datos de la tabla
+            Route::get('/producto', 'ProductoController@index');
+            Route::post('/producto/registrar', 'ProductoController@store');
+            Route::put('/producto/actualizar', 'ProductoController@update');
+            Route::put('/producto/desactivar', 'ProductoController@desactivar');
+            Route::put('/producto/activar', 'ProductoController@activar');
+            
+            //RUTAS DE LOS CLIENTES
+            //tabla clientes listar,registrar y actualizar
+            Route::get('/cliente', 'ClienteController@index');
+            Route::post('/cliente/registrar', 'ClienteController@store');
+            Route::put('/cliente/actualizar', 'ClienteController@update');
+            Route::put('/cliente/desactivar', 'ClienteController@desactivar');
+            Route::put('/cliente/activar', 'ClienteController@activar');
+            
+            //RUTAS DE LOS PROVEEDORES
+            //tabla proveedores listar, registrar y actualizar
+            Route::get('/proveedor', 'ProveedorController@index');
+            Route::post('/proveedor/registrar', 'ProveedorController@store');
+            Route::put('/proveedor/actualizar', 'ProveedorController@update');
+            Route::put('/proveedor/desactivar', 'ProveedorController@desactivar');
+            Route::put('/proveedor/activar', 'ProveedorController@activar');
+            
+            //RUTAS DE LOS ROLES
+            //roles
+            Route::get('/rol', 'RolController@index');
+            Route::get('/rol/selectRol', 'RolController@selectRol');
+            Route::post('/rol/registrar', 'RolController@store');
+            Route::put('/rol/actualizar', 'RolController@update');
+            Route::put('/rol/desactivar', 'RolController@desactivar');
+            Route::put('/rol/activar', 'RolController@activar');
+            
+            //RUTAS DE LOS USUARIOS
+            Route::get('/user', 'UserController@index');
+            Route::post('/user/registrar', 'UserController@store');
+            Route::put('/user/actualizar', 'UserController@update');
+            Route::put('/user/desactivar', 'UserController@desactivar');
+            Route::put('/user/activar', 'UserController@activar');
+            
+            
+            //////******* Rutas para la tabla de informaciones  *******/////
+            Route::get('/informacion', 'InformacionController@index');
+            Route::post('/informacion/registrar', 'InformacionController@store');
+            Route::put('/informacion/actualizar', 'InformacionController@update');
+        });
 
-//insertar registros en la tabla
-Route::post('/producto/registrar', 'ProductoController@store');
+        Route::group(['middleware' => ['Almacenero']], function(){
 
-//insertar registros en la tabla
-Route::put('/producto/actualizar', 'ProductoController@update');
-
-//desactivar registros en la tabla
-Route::put('/producto/desactivar', 'ProductoController@desactivar');
-
-//activar registros en la tabla
-Route::put('/producto/activar', 'ProductoController@activar');
-
-//RUTAS DE LOS CLIENTES
-//tabla clientes listar,registrar y actualizar
-Route::get('/cliente', 'ClienteController@index');
-Route::post('/cliente/registrar', 'ClienteController@store');
-Route::put('/cliente/actualizar', 'ClienteController@update');
-Route::put('/cliente/desactivar', 'ClienteController@desactivar');
-Route::put('/cliente/activar', 'ClienteController@activar');
-
-//RUTAS DE LOS PROVEEDORES
-//tabla proveedores listar, registrar y actualizar
-Route::get('/proveedor', 'ProveedorController@index');
-Route::post('/proveedor/registrar', 'ProveedorController@store');
-Route::put('/proveedor/actualizar', 'ProveedorController@update');
-Route::put('/proveedor/desactivar', 'ProveedorController@desactivar');
-Route::put('/proveedor/activar', 'ProveedorController@activar');
-
-//RUTAS DE LOS ROLES
-//roles
-Route::get('/rol', 'RolController@index');
-Route::get('/rol/selectRol', 'RolController@selectRol');
-Route::post('/rol/registrar', 'RolController@store');
-Route::put('/rol/actualizar', 'RolController@update');
-Route::put('/rol/desactivar', 'RolController@desactivar');
-Route::put('/rol/activar', 'RolController@activar');
-
-//RUTAS DE LOS USUARIOS
-Route::get('/user', 'UserController@index');
-Route::post('/user/registrar', 'UserController@store');
-Route::put('/user/actualizar', 'UserController@update');
-Route::put('/user/desactivar', 'UserController@desactivar');
-Route::put('/user/activar', 'UserController@activar');
+            //RUTAS DE LAS CATEGORIAS
+            Route::get('/categoria', 'CategoriaController@index');
+            Route::post('/categoria/registrar', 'CategoriaController@store');
+            Route::put('/categoria/actualizar', 'CategoriaController@update');
+            Route::put('/categoria/desactivar', 'CategoriaController@desactivar');
+            Route::put('/categoria/activar', 'CategoriaController@activar');
+            Route::get('/categoria/selectCategoria', 'CategoriaController@selectCategoria');
+            
+            
+            ///   RUTAS DE PRODUCTO ////
+            //listar todos los datos de la tabla
+            Route::get('/producto', 'ProductoController@index');
+            Route::post('/producto/registrar', 'ProductoController@store');
+            Route::put('/producto/actualizar', 'ProductoController@update');
+            Route::put('/producto/desactivar', 'ProductoController@desactivar');
+            Route::put('/producto/activar', 'ProductoController@activar');
+            
+            //RUTAS DE LOS CLIENTES
+            //tabla clientes listar,registrar y actualizar
+            Route::get('/cliente', 'ClienteController@index');
+            Route::post('/cliente/registrar', 'ClienteController@store');
+            Route::put('/cliente/actualizar', 'ClienteController@update');
+            Route::put('/cliente/desactivar', 'ClienteController@desactivar');
+            Route::put('/cliente/activar', 'ClienteController@activar');
+            
+            //RUTAS DE LOS PROVEEDORES
+            //tabla proveedores listar, registrar y actualizar
+            Route::get('/proveedor', 'ProveedorController@index');
+            Route::post('/proveedor/registrar', 'ProveedorController@store');
+            Route::put('/proveedor/actualizar', 'ProveedorController@update');
+            Route::put('/proveedor/desactivar', 'ProveedorController@desactivar');
+            Route::put('/proveedor/activar', 'ProveedorController@activar');
+            
+            //////******* Rutas para la tabla de informaciones  *******/////
+            Route::get('/informacion', 'InformacionController@index');
+            Route::post('/informacion/registrar', 'InformacionController@store');
+            Route::put('/informacion/actualizar', 'InformacionController@update');
 
 
-//////******* Rutas para la tabla de informaciones  *******/////
-Route::get('/informacion', 'InformacionController@index');
-Route::post('/informacion/registrar', 'InformacionController@store');
-Route::put('/informacion/actualizar', 'InformacionController@update');
+        });
+});
+
+//Route::get('/home', 'HomeController@index')->name('home');
