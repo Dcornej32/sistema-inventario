4f46b8a9db1df7054ccf79b3c76f1a0591f4497f
Dcornej32 <dave_cornejo@hotmail.com>
2022-05-19 23:18:21 -0600

    david branch

--- resources/assets/js/components/Proveedor.vue
+++ resources/assets/js/components/Proveedor.vue
@@ -2,7 +2,7 @@
      <main class="main">
             <!-- Breadcrumb -->
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="/">Escritorio</a></li>
+                <li class="breadcrumb-item"><a href="/">Escritorio</a></li> 
             </ol>
             <div class="container-fluid">
                 <!-- Ejemplo de tabla Listado -->
